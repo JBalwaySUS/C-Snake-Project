41bb3803f58e90c29020379bf93ebac8d1cb3339
JBalwaySUS <anirudhsankar23@gmail.com>
2024-02-26 19:36:19 +0530

    COMPLETED

--- src/snake.c
+++ src/snake.c
@@ -46,24 +46,45 @@ int main(int argc, char* argv[]) {
     // TODO: Load the board from in_filename
     // TODO: If the file doesn't exist, return -1
     // TODO: Then call initialize_snakes on the state you made
+    FILE* fp = fopen(in_filename, "r");
+    if(fp == NULL){
+      return -1;
+    }
+    else{
+      state = load_board(fp);
+      state = initialize_snakes(state);
+    }
   } else if (io_stdin) {
     // TODO: Load the board from stdin
     // TODO: Then call initialize_snakes on the state you made
+    state = load_board(stdin);
+    state = initialize_snakes(state);
   } else {
     // TODO: Create default state
+    state = create_default_state();
   }
 
   // TODO: Update state. Use the deterministic_food function
   // (already implemented in snake_utils.h) to add food.
+  update_state(state, deterministic_food);
 
   // Write updated board to file or stdout
   if (out_filename != NULL) {
     // TODO: Save the board to out_filename
+    FILE* fp = fopen(out_filename, "w");
+    if(fp == NULL){
+      return -1;
+    }
+    else{
+      print_board(state, fp);
+    }
   } else {
     // TODO: Print the board to stdout
+    print_board(state, stdout);
   }
 
   // TODO: Free the state
+  free_state(state);
 
   return 0;
 }
